5e77a2044969d1fff99186572ac47caa31997178
Kai Black <kaibblack03@gmail.com>
2023-11-08 21:09:44 -0800

    Primary files first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+if (require('electron-squirrel-startup')) app.quit();
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    icon: './SL-logo'
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
