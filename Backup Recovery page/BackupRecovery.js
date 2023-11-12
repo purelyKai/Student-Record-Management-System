1961cd49a5b4d60814447a9242a6f2848633e63e
Kai Black <kaibblack03@gmail.com>
2023-11-11 18:05:52 -0800

    Implemented HTML for Backup & Recovery page. Implemented outline for JavaScript code to be further implemented.

---
+++ Backup Recovery page/BackupRecovery.js
@@ -0,0 +1,19 @@
+// Get elements
+const backupButton = document.getElementById('backup-button');
+const recoveryButton = document.getElementById('recovery-button');
+
+// Add event listeners for when backup and recovery buttons are clicked
+backupButton.addEventListener('click', backup);
+recoveryButton.addEventListener('click', recover);
+
+function backup() {
+    // Implement backup logic here
+    console.log('Backup initiated');
+    // Add more functionality as needed
+}
+
+function recover() {
+    // Implement recovery logic here
+    console.log('Recovery initiated');
+    // Add more functionality as needed
+}
