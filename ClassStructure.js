4f167159268881d14808ada4ffb9487dea744818
Cartepay <147567497+Cartepay@users.noreply.github.com>
2023-11-19 20:04:48 -0800

    Add files via upload
    Full page of classes for use in the system. I tried to make them all properly scalable within each other through the use of foreign keys so that we can implement all required information. They are not currently populated but we can populate these classes and then draw from them as we see fit

--- ClassStructure.js
+++ ClassStructure.js
@@ -3,13 +3,74 @@
 
 class Account {
     //User accounts. Cascades into student, professor, and faculty accounts
-    constructor(Username, Password, ProfilePic, FirstName, LastName, Description){
+    constructor(userID, Username, Password, ProfilePic, FirstName, LastName, Description, dateofBirth, email){
+        this.userID = userID;
         this.Username = Username;
         this.Password = Password;
         this.ProfilePic = ProfilePic;
         this.FirstName = FirstName;
         this.LastName = LastName;
         this.Description = Description;
+        this.dateofBirth = dateofBirth;
+        this.email = email;
+
+    }
+}
+
+
+class Course{
+    //Courses registered to the school including their time slots and english name
+    constructor(courseID, courseName, startDate, endDate, courseStartTime, courseEndTime){
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.courseStartTime = courseStartTime;
+        this.courseEndTime = courseEndTime;
+    }
+}
+
+
+class AdditionalCouseOccurence{
+    //Space for odd dates and times a class may occur
+    constructor(occurenceID, dateofOccurence, startTime, endTime, courseID){
+        this.occurenceID = occurenceID;
+        this.dateofOccurence = dateofOccurence;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        Course.courseID = courseID; //gets course name from here
+
+    }
+}
+
+
+class CourseList{
+    //Table to tie specific user id's to courses. Keeps a full list of registered students and staff
+    constructor(studentCourseID, courseID, userID){
+        this.studentCourseID = studentCourseID;
+        Course.courseID = courseID;
+        Account.userID = userID;
+    }
+}
+
+
+class Grades{
+    //Space to track individual student grades per class. This system will only track final grades per class
+    constructor(courseID, userID, gradeID){
+        Course.courseID = courseID;
+        Account.userID = userID;
+        this.gradeID = gradeID;
+    }
+}
+
+
+class Attendance{
+    //Space to track individual student attendance per day, per class
+    constructor(courseID, userID, date, didAttend){
+        Course.courseID = courseID;
+        Account.userID = userID;
+        this.date = date;
+        this.didAttend = didAttend;
     }
 }
 
